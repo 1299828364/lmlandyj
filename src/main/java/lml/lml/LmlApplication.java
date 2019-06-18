a17463e225e46a58265713d03a4dcadb24c5e419
lml <18716391143@163.com>
2019-06-18 18:47:43 +0800

    Initial commit

---
+++ src/main/java/lml/lml/LmlApplication.java
@@ -0,0 +1,13 @@
+package lml.lml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lml/lml/LmlApplicationTests.java
@@ -0,0 +1,16 @@
+package lml.lml;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LmlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
